e73cf48bb89d5d10bca3a960b1d617657e5e0240
hemanthghs <ghshemanth@gmail.com>
2025-02-21 16:26:17 +0530

    update url

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,2 +1,1 @@
-// export const AGENT_BACKEND_URL = "http://170.75.255.103:3000";
-export const AGENT_BACKEND_URL = "http://localhost:3000";
+export const AGENT_BACKEND_URL = "http://170.75.255.103:3000";
